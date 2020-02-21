070ee9a3ab61d314ef4bb68e2e432ae75b58facb
Ashis Sarthak Singh <Ashissarthak.singh@refinitiv.com>
2020-02-21 18:36:59 +0530

    initial logic

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,45 @@ export default class App extends Component<{}, AppState> {
     const listOfDevices = [
       ...new Set(data.map((value: any) => value.serialNumber))
     ];
-    console.log(listOfDevices);
+    const batteryReadings: any[] = [];
+    listOfDevices.forEach((value: any) => {
+      batteryReadings[value] = data.filter(
+        (val: any) => val.serialNumber === value
+      );
+    });
+    let initialReading = 0;
+    let initIndex = 0;
+    let percentBattery: any[] = [];
+    let timeSt: Date = new Date();
+    Object.values(batteryReadings)[0].forEach((value: any) => {
+      if (!initialReading) {
+        initialReading = value.batteryLevel * 100;
+        timeSt = new Date(value.timestamp);
+      } else if (value.batteryLevel * 100 < initialReading) {
+        let newtimeSt: Date = new Date(value.timestamp);
+        const diffInHr =
+          Math.abs(newtimeSt.getTime() - timeSt.getTime()) / 36e5;
+        percentBattery[initIndex] =
+          ((initialReading - value.batteryLevel * 100) / diffInHr) * 24;
+      } else {
+        initialReading = value.batteryLevel * 100;
+        initIndex++;
+      }
+    });
+    const finalPercent = percentBattery.reduce(
+      (val: number, totalEntry: number) => {
+        return totalEntry + val;
+      },
+      0
+    );
+    const finalTime = Object.values(batteryReadings)[0][
+      Object.values(batteryReadings)[0].length - 1
+    ].timestamp;
+    console.log(
+      finalPercent /
+        (Math.abs(new Date(timeSt).getTime() - new Date(finalTime).getTime()) /
+          36e5)
+    );
   };
   render() {
     return <div>Hi</div>;
